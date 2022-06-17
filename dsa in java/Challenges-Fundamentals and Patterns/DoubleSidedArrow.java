882c6da3bb03b412ec942c3b455ae724f9157835
Suraz Karn <surazkarn8848@gmail.com>
2022-06-17 21:55:15 +0530

    DoubleSidedArrow

--- dsa in java/Challenges-Fundamentals and Patterns/DoubleSidedArrow.java
+++ dsa in java/Challenges-Fundamentals and Patterns/DoubleSidedArrow.java
@@ -1,32 +1,73 @@
+/* 
+
+7
+
+            1
+        2 1   1 2
+    3 2 1       1 2 3
+4 3 2 1           1 2 3 4
+    3 2 1       1 2 3
+        2 1   1 2
+            1 
+            
+*/
+
 import java.util.*;
 
 public class DoubleSidedArrow {
     public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        Scanner scn = new Scanner(System.in);
 
-        int nsp = n - 1;
-        int num = 1;
+        int n = scn.nextInt();
+
+        int nst = 1;
+        int nsp = 2 * n - 2;
+        int mnsp = 2;
 
         int row = 1;
+
         while (row <= n) {
-
-            // space work
-            int csp = 1;
-            while (csp <= nsp) {
-                System.out.println(" ");
-                csp++;
+            for (int csp = 1; csp <= nsp; csp++) {
+                System.out.print(" ");
             }
 
-            // number work
-            int cnum = 1;
-            while (cnum <= num) {
-                System.out.println(n);
-                cnum++;
-
+            for (int cst = nst; cst >= 1; cst--) {
+                System.out.print(cst + " ");
             }
+
+            if (row > 1 && row < n) {
+                for (int csp = 1; csp <= mnsp; csp++) {
+                    System.out.print(" ");
+                }
+
+                if (row <= n / 2) {
+                    mnsp += 4;
+                } else {
+                    mnsp -= 4;
+                }
+            }
+
+            int cst = 1;
+
+            if (row == 1 || row == n) {
+                cst = 2;
+            }
+
+            for (; cst <= nst; cst++) {
+                System.out.print(cst + " ");
+            }
+
+            System.out.println();
+
+            if (row <= n / 2) {
+                nst += 1;
+                nsp -= 4;
+            } else {
+                nst -= 1;
+                nsp += 4;
+            }
+
             row++;
         }
-
     }
 }
