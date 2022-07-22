b6edbae56cdd0de4b14c640e4e77f5f0c168535d
surazkarn <surazkarn8848@gmail.com>
2022-07-22 23:51:30 +0530

    arraylist

---
+++ dsa in java/7-ArrayList-twoD/TwoD.java
@@ -0,0 +1,24 @@
+public class TwoD {
+
+    public static void main(String[] args) {
+        // initialization
+        int[][] arr;
+        // declaration
+        arr = new int[3][3];
+
+        // System.out.println(arr[0][0]);
+        display(arr);
+
+    }
+
+    // function to display entire array
+
+    public static void display(int[][] arr) {
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+        }
+    }
+}
