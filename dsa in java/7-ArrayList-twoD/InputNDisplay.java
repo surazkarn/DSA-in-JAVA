4463b54633e0401f7bbf2b573a523d976f681102
surazkarn <surazkarn8848@gmail.com>
2022-07-24 02:11:44 +0530

    arraylist

---
+++ dsa in java/7-ArrayList-twoD/InputNDisplay.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class InputNDisplay {
+
+    public static void main(String[] args) {
+        int arr[][] = takeInput();
+
+    }
+
+    // funtion to take input
+    public static void takeInput() {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter number of rows ");
+        int rows = sc.nextInt();
+        int[][] arr = new int[rows][];
+        for (int row = 0; row < rows; row++) {
+
+        }
+    }
+
+    public static void displayOut() {
+
+    }
+}
--- dsa in java/7-ArrayList-twoD/TwoD.java
+++ dsa in java/7-ArrayList-twoD/TwoD.java
@@ -19,6 +19,7 @@ public class TwoD {
             for (int j = 0; j < arr[i].length; j++) {
                 System.out.print(arr[i][j] + " ");
             }
+            System.out.println();
         }
     }
 }
