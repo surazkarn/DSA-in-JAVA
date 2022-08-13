18e7950aa4fec57736aa159b24179adcd9488986
surazkarn <surazkarn8848@gmail.com>
2022-08-13 22:35:14 +0530

    added

---
+++ dsa in java/LinkedList/Client.java
@@ -0,0 +1,16 @@
+package LinkedList;
+
+import java.util.LinkedList;
+
+public class Client {
+    public static void main(String[] args) {
+        // Node nn = new Node();
+
+        LinkedList list = new LinkedList();
+        // if we are doing new LiNKEDlIST the space will be allocated to head tail and
+        // size
+        // if we are doing new Node the will be allocated to node and next
+
+    }
+
+}
--- dsa in java/LinkedList/First.java
+++ dsa in java/LinkedList/First.java
@@ -7,16 +7,30 @@ public class First {
     // thats y inner and private , koi vi bahar ki class update karsakey
 
     private class Node {
+        // why private?
         // two properties of Node
         int data;
         Node next;
 
+        // we should know the starting
+        // head is refrence to the first node
         // head, tail and size is the property of linked list
         private node head;
         private node tail;
         private int size;
+        // why private?
 
-        
+        // traversing the linked list
+
+        public void display() {
+            Node tempNode = this.head;
+            // refrence to class data member
+            while (tempNode != null) {
+                System.out.println(tempNode.data);
+                tempNode = tempNode.next;
+            }
+        }
+
     }
 
 }
