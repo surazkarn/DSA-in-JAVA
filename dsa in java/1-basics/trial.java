58d19ff9c88b57e39fcc66caf07bfcbad3c4fe32
surazkarn <surazkarn8848@gmail.com>
2022-08-17 01:28:37 +0530

    added

---
+++ dsa in java/1-basics/trial.java
@@ -0,0 +1,8 @@
+
+public class trial {
+    public static void main(String[] args) {
+        StringBuffer s1 = new StringBuffer("hello world");
+        s1.insert(6, "Good ");
+        System.out.println(s1);
+    }
+}
