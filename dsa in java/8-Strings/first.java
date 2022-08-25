e2947ce3e2fe75871e3d53927adf3a9f54104d2b
surazkarn <surazkarn8848@gmail.com>
2022-08-25 23:50:43 +0530

    added

---
+++ dsa in java/8-Strings/first.java
@@ -0,0 +1,11 @@
+//strings in java are immutable- u cannot make any change ur string
+public class first {
+
+    public static void main(String[] args) {
+        String str = "hello suraj";
+
+        str = "bye";
+        System.out.println(str);
+    }
+
+}
